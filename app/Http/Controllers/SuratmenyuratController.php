d1bdea5f4ac6249430f14666e60724fecf3e77b0
VLuth <vierradante@gmail.com>
2024-06-07 09:52:54 +0700

    udah

---
+++ app/Http/Controllers/SuratmenyuratController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\suratmenyurat;
+use App\Http\Requests\StoresuratmenyuratRequest;
+use App\Http\Requests\UpdatesuratmenyuratRequest;
+
+class SuratmenyuratController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoresuratmenyuratRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(suratmenyurat $suratmenyurat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(suratmenyurat $suratmenyurat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatesuratmenyuratRequest $request, suratmenyurat $suratmenyurat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(suratmenyurat $suratmenyurat)
+    {
+        //
+    }
+}
