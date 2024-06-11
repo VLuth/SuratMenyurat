4a94aa94f79b53a891b06bdb690a600b82abc6c0
VLuth <vierradante@gmail.com>
2024-06-11 09:18:57 +0700

    Dashboard

--- resources/views/template/mainskre.blade.php
+++ resources/views/template/mainskre.blade.php
@@ -1,3 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title>Surat Menyurat</title>
+	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+	<link rel="icon" href="{{asset('atlantis')}}/assets/img/icon.ico" type="image/x-icon"/>
+
+	<!-- Fonts and icons -->
+	<script src="{{asset('atlantis')}}/assets/js/plugin/webfont/webfont.min.js"></script>
+	<script>
+		WebFont.load({
+			google: {"families":["Lato:300,400,700,900"]},
+			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['{{asset('atlantis')}}/assets/css/fonts.min.css']},
+			active: function() {
+				sessionStorage.fonts = true;
+			}
+		});
+	</script>
+
+	<!-- CSS Files -->
+	<link rel="stylesheet" href="{{asset('atlantis')}}/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="{{asset('atlantis')}}/assets/css/atlantis.min.css">
+
+	<!-- CSS Just for demo purpose, don't include it in your project -->
+	<link rel="stylesheet" href="{{asset('atlantis')}}/assets/css/demo.css">
+</head>
+<body data-background-color="dark">
+	<div class="wrapper">
 <div class="main-header">
     <!-- Logo Header -->
     <div class="logo-header" data-background-color="dark2">
